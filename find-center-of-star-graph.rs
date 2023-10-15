50483767db1f7202e56729153f396bc8bcfbd586
Marcos Garcia de Lima <marckfree@gmail.com>
2023-10-14 22:49:28 -0300

    Add comments

--- find-center-of-star-graph.rs
+++ find-center-of-star-graph.rs
@@ -1,3 +1,5 @@
+// https://leetcode.com/submissions/detail/1075406484/
+
 use std::collections::{HashMap, HashSet};
 
 pub fn find_center(edges: Vec<Vec<i32>>) -> i32 {
@@ -23,6 +25,7 @@ pub fn find_center(edges: Vec<Vec<i32>>) -> i32 {
 }
 
 fn main() {
+    // Tests
     println!("{:?}", find_center(vec![vec![1, 2], vec![2, 3], vec![4, 2]]));
     println!("{:?}", find_center(vec![vec![1, 2], vec![5, 1], vec![1, 3], vec![1, 4]]));
 }
